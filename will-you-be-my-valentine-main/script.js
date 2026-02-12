842077f04194e40ef77e27fd42a6775935673b19
charliedeguzman <nyoknyok10@yahoo.com>
2026-02-12 23:21:47 +0800

    Add files via upload

---
+++ will-you-be-my-valentine-main/script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const tontonGifs = [
+  "https://media.tenor.com/TUVAE2M_wz4AAAAi/chubby-tonton.gif",
+  "https://media.tenor.com/pZk_U5JVWzUAAAAi/tonton-friends-tonton.gif",
+  "https://media.tenor.com/Jkha__Yjf0oAAAAi/sad-depression.gif",
+  "https://media.tenor.com/U0OPHZokzkUAAAAi/what-seriously.gif",
+  "https://media.tenor.com/WKXMmSk3JJsAAAAi/chubby-tonton.gif",
+  "https://media.tenor.com/ZHWV13jliTAAAAAi/chubby-tonton.gif",
+];
+
+const title = document.querySelector(".title");
+const btnContainer = document.querySelector(".buttons");
+const yesBtn = document.querySelector(".btn-yes");
+const noBtn = document.querySelector(".btn-no");
+const img = document.querySelector(".img");
+
+const MAX_IMAGES = 5;
+let play = true;
+let noCount = 0;
+let noButtonSize = 1;
+let yesButtonSize = 1;
+
+yesBtn.addEventListener("click", () => {
+  title.innerHTML = "Finally ! I Love You Love!! 💗";
+  btnContainer.classList.add("hidden");
+  changeImage("yes");
+});
+
+noBtn.addEventListener("click", () => {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    shrinkNoButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) play = false;
+  }
+});
+
+function resizeYesButton() {
+  yesButtonSize *= 1.2;
+  yesBtn.style.transform = `scale(${yesButtonSize})`;
+}
+
+function shrinkNoButton() {
+  noButtonSize *= 0.90;
+  noBtn.style.transform = `scale(${noButtonSize})`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No 😔",
+    "Are you sure Lyle?? 🥺",
+    "please 🥹",
+    "Don't do this to me 😭",
+    "You're breaking my heart 💔",
+    "I'm gonna cry... 😭💔",
+  ];
+  return messages[Math.min(noCount, messages.length - 1)];
+}
+
+function changeImage(image) {
+  img.src =
+    image === "yes"
+      ? "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2c4amg3M2V2bWdidTZpNWN4aW1waXFqc3hseXpkaG9vODlwNm40cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/scuNkdsX0mydc7T02b/giphy.gif"
+      : tontonGifs[image];
+}
+
+function updateNoButtonText() {
+  noBtn.innerHTML = generateMessage(noCount);
+}
